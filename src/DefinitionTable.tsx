2b4baad61ca38956b4350927807cb2e8bc840743
chrjl <73675826+chrjl@users.noreply.github.com>
2023-10-28 17:31:46 -0700

    style(DefinitionTable): display grid

--- src/DefinitionTable.tsx
+++ src/DefinitionTable.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import { ResumeEntry } from '@reactresume/types';
+
+const Container = styled.dl`
+  display: grid;
+  grid-template-columns: 12em auto;
+  align-items: center;
+  column-gap: 2em;
+  row-gap: 0.5em;
+`;
+
+const Title = styled.dt`
+  text-transform: uppercase;
+  font-weight: bold;
+`;
+
+const Description = styled.dd`
+`;
 
 interface DefinitionTableProps {
   data: ResumeEntry[];
 }
 
 export const DefinitionTable = ({ data }: DefinitionTableProps) => (
-  <dl>
+  <Container>
     {data.map(({ title, description }, index) => (
       <React.Fragment key={index}>
-        <dt>{title}</dt>
-        <dd>{description}</dd>
+        <Title>{title}</Title>
+        <Description>{description}</Description>
       </React.Fragment>
     ))}
-  </dl>
+  </Container>
 );
